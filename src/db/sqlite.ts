3056ca0782ee995bb7729f062be142cb91050a3a
Deer man <1141943639@qq.com>
2023-01-17 23:33:03 +0800

    开始创建数据库

--- src/db/sqlite.ts
+++ src/db/sqlite.ts
@@ -1,13 +1,29 @@
 import config from 'config';
+import { ensureFileSync } from 'fs-extra';
 import knex from 'knex';
+import { resolve } from 'path';
+
+const path = resolve('./', 'db', `${config.SQLITE_FILE_NAME}.sqlite3`);
+ensureFileSync(path);
 
 const sqlite = knex({
   client: 'sqlite3',
-  connection: () => ({
-    filename: config.SQLITE_FILE_NAME,
-    asyncStackTraces: config.NODE_ENV === 'develop',
-    debug: config.NODE_ENV === 'develop',
-  }),
+  connection: {
+    filename: path,
+  },
+  useNullAsDefault: true,
+  debug: config.NODE_ENV === 'develop',
+  asyncStackTraces: config.NODE_ENV === 'develop',
 });
+
+(async () => {
+  try {
+    await sqlite.raw('SELECT 1');
+    console.log('sqlite数据库连接成功');
+  } catch (err) {
+    console.log('sqlite数据库连接失败');
+    console.error(err);
+  }
+})();
 
 export default sqlite;
--- src/model/user_modal.ts
+++ src/model/user_modal.ts
@@ -1,8 +1,10 @@
 import sqlite from 'db/sqlite';
 import { USER } from 'common/table_name';
 
-sqlite.schema.createTable(USER, (table) => {
-  table.increments('id');
-  table.string('username').unique().notNullable().comment('用户名, 唯一');
-  table.string('password').notNullable().comment('密码');
-});
+sqlite.schema
+  .createTable(USER, (table) => {
+    table.increments('id');
+    table.string('username').unique().notNullable().comment('用户名, 唯一');
+    table.string('password').notNullable().comment('密码');
+  })
+  .then();
